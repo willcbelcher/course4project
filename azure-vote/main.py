6ea49ca4acb9b3170b49d39579756cd056a748ab
William Belcher <willcbelcher@gmail.com>
2021-06-30 18:48:59 -0400

    trying to fix VMSS Deployment

--- azure-vote/main.py
+++ azure-vote/main.py
@@ -120,8 +120,8 @@ def index():
             # Return results
             return render_template("index.html", value1=int(vote1), value2=int(vote2), button1=button1, button2=button2, title=title)
 
-if __name__ == "__main__":
+# if __name__ == "__main__":
     # comment line below when deploying to VMSS
     # app.run() # local
     # uncomment the line below before deployment to VMSS
-    app.run(host='0.0.0.0', threaded=True, debug=True) # remote
+app.run(host='0.0.0.0', threaded=True, debug=True) # remote
